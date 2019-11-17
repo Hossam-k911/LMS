33b969f44a69206a1568dc9b2b2dd5511fe1bcdd
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2019-11-17 19:25:16 +0200

    heroku db connection

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -9,10 +9,18 @@
 //   );
 // };
 // //"mongodb+srv://LMS_db:123@cluster0-2uup7.mongodb.net/lms?retryWrites=true&w=majority"
-require('dotenv').config({ path: 'MONGODB_URI' });
+require('dotenv').config();
+
 const mongoose = require("mongoose");
+
 module.exports = function dbConnection() {
-  mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true })
+  mongoose.Promise = global.Promise;
+  mongoose.connect(process.env.MONGODB_URI,  {useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true ,
+    auth: {
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASSWORD
+    }
+  })
     .then(db => console.log(`DB is connected`))
     .catch(err => console.error(err));
 };
