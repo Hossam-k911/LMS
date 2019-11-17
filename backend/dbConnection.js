068b3d9c9ec912e2fe69955961ffcb835001a5b5
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2019-11-17 16:37:26 +0200

    heroku

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,7 +1,18 @@
-var mongoose = require('mongoose')
-
-
-module.exports = function (){
-    mongoose.Promise = global.Promise;
-    mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://LMS_db:123@cluster0-2uup7.mongodb.net/lms?retryWrites=true&w=majority',{ useUnifiedTopology: true ,useNewUrlParser:true})
-}
+// var mongoose = require("mongoose");
+// //  require('./.env/config')
+// module.exports = function() {
+//   mongoose.Promise = global.Promise;
+//   mongoose.connect(
+//      process.env.MONGODB_URI
+//       ,
+//     { useUnifiedTopology: true, useNewUrlParser: true }
+//   );
+// };
+// //"mongodb+srv://LMS_db:123@cluster0-2uup7.mongodb.net/lms?retryWrites=true&w=majority"
+require('dotenv').config({ path: 'MONGODB_URI' });
+const mongoose = require("mongoose");
+module.exports = function dbConnection() {
+  mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true })
+    .then(db => console.log(`DB is connected`))
+    .catch(err => console.error(err));
+};
