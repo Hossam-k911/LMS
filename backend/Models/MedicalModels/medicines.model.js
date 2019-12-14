004635fcf701bc467c93555761a6a00f6cfa8b76
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2019-12-14 19:19:10 +0200

    ..

--- backend/Models/MedicalModels/medicines.model.js
+++ backend/Models/MedicalModels/medicines.model.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose')
 
 var MedecinesModel = new mongoose.model('Medicines',{
     _id:mongoose.Schema.Types.ObjectId,
-    medicineName:String,
-    medicinePrice:String,
-    medicineDescription:String,
-    medicineQuantity:Number,
+    medicine_Name:String,
+    medicine_Price:String,
+    medicine_Description:String,
+    medicine_Quantity:Number,
 
     patient:{ type: mongoose.Schema.Types.ObjectId, ref: 'Patients'},
 })
