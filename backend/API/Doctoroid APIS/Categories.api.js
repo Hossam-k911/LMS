7388948a8c4641288c22de14c668fea0d7bb9700
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-01-21 19:14:25 +0200

    Categories version 1

--- backend/API/Doctoroid APIS/Categories.api.js
+++ backend/API/Doctoroid APIS/Categories.api.js
@@ -3,15 +3,21 @@ var CategoriesModel = require("../../Models/MedicalModels/Categories.model");
 module.exports = function(app) {
   app.post("/addcategory", async (req, resp) => {
     try {
-      const { category_title ,category_imgLink} = req.body;
+      const { category_title, category_imgLink } = req.body;
       let Category = new CategoriesModel({
-          _id:mongoose.Types.ObjectId(),
-          category_title,category_imgLink
+        _id: mongoose.Types.ObjectId(),
+        category_title,
+        category_imgLink
       });
       await Category.save();
       resp.status(200).json(Category);
     } catch (err) {
-        resp.status(400).json("error adding Category");
+      resp.status(400).json("error adding Category");
     }
+  });
+
+  app.get("/listcategories", async (req, resp) => {
+    let categories = await CategoriesModel.find({});
+    resp.json(categories);
   });
 };
