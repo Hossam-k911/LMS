6b3eb0898f906a42dc8ca26ac715995bee86445e
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-01-21 20:32:00 +0200

    categories version 2

--- backend/Models/MedicalModels/Categories.model.js
+++ backend/Models/MedicalModels/Categories.model.js
@@ -4,6 +4,15 @@ let CategoriesModel = new mongoose.model("Categories", {
     _id: mongoose.Schema.Types.ObjectId,
     category_title : String,
     category_imgLink: String,
+    category_medical_tests:[
+      {
+        // test_id:Number,
+        test_title:String,
+        test_period:Number,
+        test_price:Number,
+        test_description:String
+      }
+    ]
     
   });
 module.exports = CategoriesModel
