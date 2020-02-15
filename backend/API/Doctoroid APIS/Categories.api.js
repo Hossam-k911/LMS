1cf408e10ec50e2f12b22012a4d5cd330e20bee6
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-02-15 20:51:54 +0200

    authentication working

--- backend/API/Doctoroid APIS/Categories.api.js
+++ backend/API/Doctoroid APIS/Categories.api.js
@@ -20,21 +20,29 @@ module.exports = function(app) {
     let categories = await CategoriesModel.find({});
     resp.json(categories);
   });
-  app.post('/tests',async (req,resp)=>{
-      try{
-        const{cat_id,/*test_id,*/test_title,test_period,test_price,test_description}=req.body;
-        let Selectedcategory = await CategoriesModel.findOne({_id:cat_id})
-        if(Selectedcategory){
-            // let test = await category.category_medical_tests.findIndex(p=>p.test_id==test_id);
-            Selectedcategory.category_medical_tests.push({test_title,test_period,test_price,test_description});
-            
-        }
-         await Selectedcategory.save();
-        resp.status(200).json(Selectedcategory)
+  app.post("/tests", async (req, resp) => {
+    try {
+      const {
+        cat_id,
+        /*test_id,*/ test_title,
+        test_period,
+        test_price,
+        test_description
+      } = req.body;
+      let Selectedcategory = await CategoriesModel.findOne({ _id: cat_id });
+      if (Selectedcategory) {
+        // let test = await category.category_medical_tests.findIndex(p=>p.test_id==test_id);
+        Selectedcategory.category_medical_tests.push({
+          test_title,
+          test_period,
+          test_price,
+          test_description
+        });
       }
-      catch(err){
-        resp.status(400).json("error adding Tests");
-
-      }
-  })
+      await Selectedcategory.save();
+      resp.status(200).json(Selectedcategory);
+    } catch (err) {
+      resp.status(400).json("error adding Tests");
+    }
+  });
 };
--- backend/API/general APIS/patient.api.js
+++ backend/API/general APIS/patient.api.js
@@ -15,8 +15,8 @@ module.exports = function createPatientsAPIS(app) {
   app.post("/getpatient", async (req, resp) => {
     try {
       const { p_id } = req.body;
-      let patients = await patientModel.find({_id:p_id});
-      resp.status(200).send({patients});
+      let patients = await patientModel.find({ _id: p_id });
+      resp.status(200).send(patients);
     } catch (err) {
       resp.status(400).send("check patient ID");
     }
@@ -36,8 +36,6 @@ module.exports = function createPatientsAPIS(app) {
   //   let output = await patientModel.remove({});
   //   resp.json({ message: "success", result: output });
   // });
-
- 
 
   app.post("/updatepatient", async (req, resp) => {
     try {
@@ -73,5 +71,58 @@ module.exports = function createPatientsAPIS(app) {
     } catch (err) {
       resp.status(400).json("error in editing patient");
     }
+  });
+
+  app.put("/edit/:id", async (req, resp) => {
+    try {
+      var id = req.params.id;
+      patientModel.findOne({ _id: id }, function(err, foundObject) {
+        if (err) {
+          resp.status(500).json("error 1");
+        } else {
+          if (!foundObject) {
+            resp.status(404).send();
+          } else {
+            if (req.body.firstName) {
+              foundObject.firstName = req.body.firstName;
+            }
+            if (req.body.lastName) {
+              foundObject.lastName = req.body.lastName;
+            }
+            if (req.body.email) {
+              foundObject.email = req.body.email;
+            }
+            if (req.body.password) {
+              foundObject.password = req.body.password;
+            }
+            if (req.body.phone) {
+              foundObject.phone = req.body.phone;
+            }
+            if (req.body.gender) {
+              foundObject.gender = req.body.gender;
+            }
+            if (req.body.date) {
+              foundObject.date = req.body.date;
+            }
+            if (req.body.location) {
+              foundObject.location = req.body.location;
+            }
+            if (req.body.snn) {
+              foundObject.snn = req.body.snn;
+            }
+            if (req.body.medicines) {
+              foundObject.medicines = req.body.medicines;
+            }
+            foundObject.save(function(err, updateObject) {
+              if (err) {
+                resp.status(500).send();
+              } else {
+                resp.send(updateObject);
+              }
+            });
+          }
+        }
+      });
+    } catch (err) {}
   });
 };
