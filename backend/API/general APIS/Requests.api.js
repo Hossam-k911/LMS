2c0c03c9dd2a11c2b7b3c0fa6056797e88fed621
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-02-21 15:05:23 +0200

    get requests by patient ID

--- backend/API/general APIS/Requests.api.js
+++ backend/API/general APIS/Requests.api.js
@@ -1,16 +1,25 @@
 var mongoose = require("mongoose");
 var RequestsModel = require("../../Models/MedicalModels/Requests.model");
 var CategoriesModel = require("../../Models/MedicalModels/Categories.model");
-var PatientsModel = require("../../Models/MedicalModels/patient.model")
+var PatientsModel = require("../../Models/MedicalModels/patient.model");
 module.exports = function(app) {
   app.post("/addreq", async (req, resp) => {
     try {
-      const { c_id, t_id, req_status, req_time, req_answers,req_test,p_id} = req.body;
+      const {
+        c_id,
+        t_id,
+        req_status,
+        req_time,
+        req_answers,
+        req_test,
+        p_id
+      } = req.body;
       let Request = new RequestsModel({
         _id: mongoose.Types.ObjectId(),
         req_status,
         req_time,
-        req_answers,req_test
+        req_answers,
+        req_test
       });
       let Selectedcategory = await CategoriesModel.findOne({ _id: c_id });
       // let SelectedTest = await Selectedcategory.category_medical_tests[0].findOne({
@@ -18,31 +27,27 @@ module.exports = function(app) {
       // });
 
       // let SelectedTest = Selectedcategory.category_medical_tests[0].id;
-       let SelectedTest = Selectedcategory.category_medical_tests[0];
+      let SelectedTest = Selectedcategory.category_medical_tests[0];
 
       if (SelectedTest.id == t_id) {
-      Request.req_test = SelectedTest.test_title;
-      }else{
-      resp.status(400).json("check test ID");
-
+        Request.req_test = SelectedTest.test_title;
+      } else {
+        resp.status(400).json("check test ID");
       }
 
       await Request.save();
-      
+
       let SelectedPatient = await PatientsModel.findOne({ _id: p_id });
-     SelectedPatient.requests.push(Request.id)
-    await SelectedPatient .save();
+      SelectedPatient.requests.push(Request.id);
+      await SelectedPatient.save();
 
-
-    //    let selectedPatient = await PatientsModel.findOne({_id:p_id})
-    //   // let selectedPatient = await PatientsModel.findOneAndUpdate({_id:p_id},{requests:Request.id})
-    //   let output =  selectedPatient.requests.push(Request.id);
-    // //   await  output.save();
-    // await output.save()
-
+      //    let selectedPatient = await PatientsModel.findOne({_id:p_id})
+      //   // let selectedPatient = await PatientsModel.findOneAndUpdate({_id:p_id},{requests:Request.id})
+      //   let output =  selectedPatient.requests.push(Request.id);
+      // //   await  output.save();
+      // await output.save()
 
       resp.status(200).json(Request);
-      
     } catch (err) {
       resp.status(400).json(" error requesting ");
     }
@@ -53,5 +58,17 @@ module.exports = function(app) {
     resp.json(Req);
   });
 
-};
+  app.post("/patientreq", async (req, resp) => {
+   try{
+    const {p_id} = req.body;
+    let SelectedPatient = await PatientsModel.findOne({_id:p_id})
+    let requests = SelectedPatient.requests;
+    resp.status(200).json({requests});
+    
 
+   }catch(err){
+    resp.status(400).json(" error getting requests for this Patient ");
+
+   }
+  });
+};
