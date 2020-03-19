4bb8813cfe4b2ea30205193d49c48bc8f300192c
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-03-19 19:55:29 +0200

    methods allowed

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,10 @@ app.use(
 app.use(function (req, resp, next) {
   resp.header("Access-Control-Allow-Origin", "*");
   resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
+  resp.headers('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
   next();
 })
+
 app.use(cookieParser());
 app.use(
   cors(
