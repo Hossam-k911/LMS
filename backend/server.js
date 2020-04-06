0d9fff7e5b1416efd3809ad4ff5c561b20ffeb2e
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-06 17:11:59 +0200

    aa

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ app.options("/*", function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.sendStatus(200);
+  next();
 });
 
 app.all('*', function (req, res, next) {
