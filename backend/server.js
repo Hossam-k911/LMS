d7290337682f675736049d6d21df5776f7c04d08
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-06 07:30:15 +0200

    ss

--- backend/server.js
+++ backend/server.js
@@ -23,36 +23,41 @@ app.use(
     saveUninitialized: true
   })
 );
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//   // Website you wish to allow to connect
-//   res.setHeader({ 'Access-Control-Allow-Origin': true }, 'http://localhost:4200');
-//   // res.setHeader('Access-Control-Allow-Origin', true);
+  // Website you wish to allow to connect
+  res.setHeader({ 'Access-Control-Allow-Origin': '*' }, 'http://localhost:4200');
+  // res.setHeader('Access-Control-Allow-Origin', true);
 
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-//   // Pass to next layer of middleware
-//   next();
-// });
-app.use(
-  cors(
-    {
-      credentials: true,
-      origin: "http://localhost:4200",
-      // // allowedHeaders: true,
-      "x-auth-token": true
+  // Pass to next layer of middleware
+  next();
+});
+// app.use(
+//   cors(
+//     {
+//       credentials: true,
+//       origin: "http://localhost:4200",
+//       // // allowedHeaders: true,
+//       // "Access-Control-Allow-Origin": {
+//       //   "x-auth-token": true,
+//       //   "Access-Control-Allow-Headers": true,
+//       //   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+//       // },
 
-    }
-  )
-);
+
+//     }
+//   )
+// );
 
 // app.use(function (req, resp, next) {
 //   resp.header("Access-Control-Allow-Origin", "*");
