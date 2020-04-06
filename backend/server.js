84f4f063058f354138844465918ee592182b08ab
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-06 07:31:39 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  EnableCorsAttribute('http://localhost:4200');
+
   // Pass to next layer of middleware
   next();
 });
