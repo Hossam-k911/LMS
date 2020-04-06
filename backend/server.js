3060340b7f920b0d4fa48f4995cc8bc9a6cf394f
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-06 07:09:49 +0200

    func

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,35 @@ app.use(
     {
       credentials: true,
       origin: "http://localhost:4200",
-      // allowedHeaders: true,
-      "Access-Control-Allow-Origin": {
-        "x-auth-token": true,
-        "Access-Control-Allow-Headers": true,
-        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
-      },
+      // // allowedHeaders: true,
+      // "Access-Control-Allow-Origin": {
+      //   "x-auth-token": true,
+      //   "Access-Control-Allow-Headers": true,
+      //   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+      // },
 
 
     }
   )
 );
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // app.use(function (req, resp, next) {
 //   resp.header("Access-Control-Allow-Origin", "*");
 //   resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
