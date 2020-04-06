4b0b45cfb98685ab94edc302887424f5dc60b92b
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-06 16:39:23 +0200

    ss

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,32 @@ app.use(
 app.use(
   cors(
     {
-      credentials: true,
-      origin: "http://localhost:4200",
+      // credentials: true,
+      // origin: "http://localhost:4200",
 
       // allowedHeaders: true
     }
   )
 );
+/ Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // app.use(function (req, resp, next) {
 //   resp.header("Access-Control-Allow-Origin", "*");
 //   resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
