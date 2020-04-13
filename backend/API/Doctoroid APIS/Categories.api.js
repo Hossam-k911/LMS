47edd4271b46fe45ed04ea85a1dd52e10511007f
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-13 20:29:54 +0200

    delete category api

--- backend/API/Doctoroid APIS/Categories.api.js
+++ backend/API/Doctoroid APIS/Categories.api.js
@@ -1,6 +1,9 @@
 var mongoose = require("mongoose");
 var CategoriesModel = require("../../Models/MedicalModels/Categories.model");
 module.exports = function categoriesAPI(app) {
+
+
+  // Add new Medical Category 
   app.post("/addcategory", async (req, resp) => {
     try {
       const { category_title, category_imgLink } = req.body;
@@ -17,11 +20,15 @@ module.exports = function categoriesAPI(app) {
     }
   });
 
+  // Fetching all categories from Categories Collection
   app.get("/listcategories", async (req, resp) => {
     let categories = await CategoriesModel.find({});
     resp.json(categories);
   });
-  app.post("/tests", async (req, resp) => {
+
+
+  // add new Medical test to specific category
+  app.post("/addtest", async (req, resp) => {
     try {
       const {
         cat_id,
@@ -34,7 +41,7 @@ module.exports = function categoriesAPI(app) {
       let Selectedcategory = await CategoriesModel.findOne({ _id: cat_id });
       if (Selectedcategory) {
         Selectedcategory.category_medical_tests.push({
-          _id:mongoose.Types.ObjectId(),
+          _id: mongoose.Types.ObjectId(),
           test_title,
           test_period,
           test_price,
@@ -48,4 +55,78 @@ module.exports = function categoriesAPI(app) {
       resp.status(400).json("error adding Tests");
     }
   });
+
+  //delete Category
+  app.post('/delcategory', async (req, resp) => {
+    try {
+      const { cat_id } = req.body
+      await CategoriesModel.remove({ _id: cat_id });
+      resp.status(200).json("success");
+
+    } catch (err) {
+      resp.status(400).send("error .. check Category ID ");
+
+    }
+  })
+
+  //delete specific test from category
+  app.post("/deltest", async (req, resp) => {
+    try {
+      const { cat_id, t_id } = req.body;
+      let Selectedcategory = await CategoriesModel.findOne({ _id: cat_id });
+      let arr = [];
+      let index = 0;
+      for (i = 0; i < Selectedcategory.category_medical_tests.length; i++) {
+        if (Selectedcategory.category_medical_tests[i].id != t_id) {
+          arr[index] = Selectedcategory.category_medical_tests[i]
+          index++;
+        }
+      }
+      Selectedcategory.category_medical_tests = arr;
+      await Selectedcategory.save();
+      resp.status(200).json("success");
+    } catch (err) {
+      resp.status(400).send("error .. check request ID ");
+    }
+  });
+  app.put(`/editcategory/:cat_id`, async (req, resp) => {
+    try {
+
+      const { cat_id } = req.params;
+      const { t_id } = req.body;
+      await CategoriesModel.findOne({ _id: cat_id }, function (err, foundObject) {
+        if (err) {
+          resp.status(500).json("error 1");
+        } else {
+          if (req.body.category_title) {
+            foundObject.category_title = req.body.category_title;
+          }
+          if (req.body.category_imgLink) {
+            foundObject.category_imgLink = req.body.category_imgLink;
+          }
+
+          for (i = 0; i < category_medical_tests.length; i++) {
+            if (category_medical_tests[i].id = t_id) {
+              if (req.body.category_medical_tests.test_title) {
+                foundObject.category_medical_tests.test_title = req.body.category_medical_tests.test_title;
+              }
+            }
+          }
+
+          foundObject.save(function (err, updateObject) {
+            if (err) {
+              resp.status(500).send();
+            } else {
+              resp.send(updateObject);
+            }
+          });
+        }
+      });
+
+
+
+    } catch (err) { }
+  });
+
+
 };
