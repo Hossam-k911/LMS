953be6794a8408c4a6e150bd4db83dad4487d46b
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-04-24 20:36:46 +0200

    editing hirarchy + edit patient new api

--- backend/API/index.js
+++ backend/API/index.js
@@ -1,14 +1,14 @@
- function RunAPIS(app){
+function RunAPIS(app) {
     require('./general APIS/patient.api')(app)
     require('./general APIS/medecine.api')(app)
     require('./Doctoroid APIS/Register.api')(app)
-    require('./general APIS/Authentication.api')(app)
+    require('./Doctoroid APIS/Authentication.apipi')(app)
     require('./general APIS/hospital.api')(app)
-    require('./Doctoroid APIS/Categories.api')(app)
+    require('./general APIS/Categories.api')(app)
     require('./Dashboard APIS/AdminAuth.api')(app)
     require('./general APIS/Requests.api')(app)
 
-    
+
 
 }
 module.exports = RunAPIS
