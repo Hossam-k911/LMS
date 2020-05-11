309083f88a37edde040ff0bd0c60230dae203598
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-05-11 06:17:32 +0200

    body-parser payload

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,13 @@ app.all('*', function (req, res, next) {
 app.options("*", cors(),
   app.use(Authenticate)
 );
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
 app.use(methodOverride('_method'));
-app.use(express.json());
+// app.use(express.json());
 app.use(
   session({
     genid: uuid,
